e7b303f210f5ac7299e02c9a7fadd4da58022948
Huzaifa Arshad <huzaifaarshad08@gmail.com>
2024-01-09 11:04:56 +0500

    Added New Directories

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,11 @@
+"""
+Test custom Django management command
+"""
+
+from unittest import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
